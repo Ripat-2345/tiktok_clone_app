8ff2f209875b35a9cc36505ae0237ffc0bb2ccfe
Ripat-2345 <ripatalsafar@gmail.com>
2022-01-15 23:33:15 +0700

    login ui screen clear

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone_app/constants.dart';
+import 'package:tiktok_clone_app/views/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor,
       ),
-      home: const Text("Hello World!"),
+      home: LoginScreen(),
     );
   }
 }
