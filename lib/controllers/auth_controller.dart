a82ddf248052e5b468f9efdcac36c6dd83670c08
Ripat-2345 <ripatalsafar@gmail.com>
2022-01-17 13:37:08 +0700

    User model done

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,3 +1,41 @@
-import 'package:get/get.dart';
+import 'dart:io';
 
-class AuthController extends GetxController {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:get/get.dart';
+import 'package:tiktok_clone_app/constants.dart';
+
+class AuthController extends GetxController {
+  // * Upload to firebase storage
+  Future<String> _uploadToStorage(File image) async {
+    Reference ref = firebaseStorage
+        .ref()
+        .child("profilePics")
+        .child(firebasAuth.currentUser!.uid);
+
+    UploadTask uploadTask = ref.putFile(image);
+    TaskSnapshot snap = await uploadTask;
+    String downloadUrl = await snap.ref.getDownloadURL();
+    return downloadUrl;
+  }
+
+  // * Register new user
+  void registerUser(
+      String username, String email, String password, File? image) async {
+    try {
+      if (username.isNotEmpty &&
+          email.isNotEmpty &&
+          password.isNotEmpty &&
+          image != null) {
+        // * Save out user to our auth and firebse firestore
+        UserCredential cred = await firebasAuth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        String downloadUrl = await _uploadToStorage(image);
+      }
+    } catch (e) {
+      Get.snackbar("Error Creating Account", e.toString());
+    }
+  }
+}
